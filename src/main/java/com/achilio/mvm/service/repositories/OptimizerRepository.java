0229408d76305e5d37867c4dbbde760bd18d8574
Esteban Gonzalez <esteban@XPS-13-7390-2-in-1-2aa769d7.localdomain>
2021-10-21 20:16:44 +0200

    remove jpa repository and fix build

--- src/main/java/com/achilio/mvm/service/repositories/OptimizerRepository.java
+++ src/main/java/com/achilio/mvm/service/repositories/OptimizerRepository.java
@@ -2,6 +2,7 @@ package com.achilio.mvm.service.repositories;
 
 import com.achilio.mvm.service.entities.Optimization;
 import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,4 @@ import org.springframework.stereotype.Repository;
 public interface OptimizerRepository extends JpaRepository<Optimization, Integer> {
 
   Optional<Optimization> findAllByProjectId(String projectId);
-
-  Optional<Optimization> findAllByProjectIdDateAfter(String projectId, ZonedDateTime time);
 }
