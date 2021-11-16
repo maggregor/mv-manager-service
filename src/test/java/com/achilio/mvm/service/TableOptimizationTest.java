5127087550ffe5b59cd41c29b0b62a885fc435d4
estebgonza <esteban.gonzalez@achilio.com>
2021-11-16 20:33:21 +0100

    test empty constructor

--- src/test/java/com/achilio/mvm/service/TableOptimizationTest.java
+++ src/test/java/com/achilio/mvm/service/TableOptimizationTest.java
@@ -1,6 +1,7 @@
 package com.achilio.mvm.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import com.achilio.mvm.service.entities.TableOptimization;
 import org.junit.Test;
@@ -11,12 +12,20 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class TableOptimizationTest {
 
   @Test
-  public void gettersAsserts() {
+  public void constructorWithArguments() {
     TableOptimization tableOptimization = new TableOptimization("myProject", "myDataset",
         "myTable");
     assertEquals("myProject", tableOptimization.getProjectId());
     assertEquals("myDataset", tableOptimization.getDatasetName());
     assertEquals("myTable", tableOptimization.getTableName());
+  }
+
+  @Test
+  public void constructorEmpty() {
+    TableOptimization tableOptimization = new TableOptimization();
+    assertNull(tableOptimization.getProjectId());
+    assertNull(tableOptimization.getDatasetName());
+    assertNull(tableOptimization.getTableName());
   }
 
 }
