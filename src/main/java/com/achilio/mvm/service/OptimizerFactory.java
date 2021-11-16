a81ce36f88f4d47901508b59cefddb043493593b
estebgonza <esteban.gonzalez@achilio.com>
2021-11-16 22:26:44 +0100

    add prive OptimizerFactory constructor

--- src/main/java/com/achilio/mvm/service/OptimizerFactory.java
+++ src/main/java/com/achilio/mvm/service/OptimizerFactory.java
@@ -1,7 +1,10 @@
 package com.achilio.mvm.service;
 
 public class OptimizerFactory {
-  
+
+  private OptimizerFactory() {
+  }
+
   public static Optimizer createOptimizer(int maxFieldSet) {
     return new BruteForceOptimizer(maxFieldSet);
   }
