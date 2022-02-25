0e903c736c065be7ca98e83865e7ae940a07bef8
estebgonza <esteban.gonzalez@achilio.com>
2022-02-25 13:33:26 +0100

    remove comment

--- src/main/java/com/achilio/mvm/service/configuration/SecurityConfig.java
+++ src/main/java/com/achilio/mvm/service/configuration/SecurityConfig.java
@@ -23,7 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .csrf()
         .disable()
         .antMatcher("/api/**");
-    //        .addFilterAfter(new AccessTokenFilter(), BasicAuthenticationFilter.class);
   }
 
   @Bean
