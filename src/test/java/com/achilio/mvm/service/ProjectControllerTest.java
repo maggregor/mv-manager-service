6c7b82fbd85a5901e5377ce3e91b214395afcc99
NicoGGG <coolranger@protonmail.com>
2022-02-26 17:47:37 +0100

    test controler get project working for now

--- src/test/java/com/achilio/mvm/service/ProjectControllerTest.java
+++ src/test/java/com/achilio/mvm/service/ProjectControllerTest.java
@@ -1,64 +1,57 @@
 package com.achilio.mvm.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.achilio.mvm.service.configuration.AccessTokenInterceptor;
-import com.achilio.mvm.service.configuration.GoogleProjectInterceptor;
 import com.achilio.mvm.service.controllers.ProjectController;
+import com.achilio.mvm.service.controllers.responses.ProjectResponse;
 import com.achilio.mvm.service.databases.entities.DefaultFetchedProject;
+import com.achilio.mvm.service.entities.Project;
 import com.achilio.mvm.service.services.FetcherService;
-import java.io.IOException;
-import org.junit.Ignore;
+import com.achilio.mvm.service.services.ProjectService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 public class ProjectControllerTest {
 
-  @MockBean AccessTokenInterceptor accessTokenInterceptor;
-  @MockBean GoogleProjectInterceptor googleProjectInterceptor;
-  @MockBean FetcherService mockedFetcherService;
-  @Autowired private ProjectController controller;
-  @Autowired private MockMvc mockMvc;
-
-  @BeforeEach
-  void initTest() throws IOException {
-    when(accessTokenInterceptor.preHandle(any(), any(), any())).thenReturn(true);
-    when(googleProjectInterceptor.preHandle(any(), any(), any())).thenReturn(true);
-    // other stuff
-  }
+  private final ObjectMapper objectMapper = new ObjectMapper();
+  @InjectMocks ProjectController controller;
+  @Mock FetcherService mockedFetcherService;
+  @Mock ProjectService mockedProjectService;
 
   @Test
   public void contextLoads() {
     assertThat(controller).isNotNull();
   }
 
-  // WIP: Not working
   @Test
-  @Ignore
-  public void shouldReturnDefaultMessage() throws Exception {
-    when(mockedFetcherService.fetchProject("achilio-dev"))
-        .thenReturn(new DefaultFetchedProject("achilio-dev", "Achilio Dev"));
-    this.mockMvc
-        .perform(get("/api/v1/project/{projectId}", "achilio-dev"))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(content().string(containsString("Hello, World")));
+  public void getProject() throws JsonProcessingException {
+    String expectedResponse1 =
+        "{\"projectId\":\"achilio-dev\",\"projectName\":\"Achilio Dev\",\"username\":\"\",\"mvMaxPerTable\":20,\"analysisTimeframe\":30,\"activated\":false,\"automatic\":false}";
+    Project project = new Project("achilio-dev");
+    DefaultFetchedProject fetchedProject = new DefaultFetchedProject("achilio-dev", "Achilio Dev");
+    when(mockedFetcherService.fetchProject(any())).thenReturn(fetchedProject);
+    when(mockedProjectService.findProjectOrCreate(any())).thenReturn(project);
+    ProjectResponse responseEntity = controller.getProject("achilio-dev");
+    String jsonResponse = objectMapper.writeValueAsString(responseEntity);
+    assertEquals(expectedResponse1, jsonResponse);
+
+    String expectedResponse2 =
+        "{\"projectId\":\"achilio-dev\",\"projectName\":\"Achilio Dev\",\"username\":\"myEmail\",\"mvMaxPerTable\":20,\"analysisTimeframe\":30,\"activated\":false,\"automatic\":false}";
+    project.setUsername("myEmail");
+    responseEntity = controller.getProject("achilio-dev");
+    jsonResponse = objectMapper.writeValueAsString(responseEntity);
+    assertEquals(expectedResponse2, jsonResponse);
   }
 }
