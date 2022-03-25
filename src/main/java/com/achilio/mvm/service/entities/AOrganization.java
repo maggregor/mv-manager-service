913ebfdb9a24ee737a0621b20205f2465bf3580b
NicoGGG <coolranger@protonmail.com>
2022-03-25 13:05:32 +0100

    Enum string for AOrg Type

--- src/main/java/com/achilio/mvm/service/entities/AOrganization.java
+++ src/main/java/com/achilio/mvm/service/entities/AOrganization.java
@@ -2,6 +2,8 @@ package com.achilio.mvm.service.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -20,7 +22,9 @@ public class AOrganization {
 
   @Column private String googleWorkspaceId;
 
-  @Column private OrganizationType organizationType;
+  @Enumerated(EnumType.STRING)
+  @Column
+  private OrganizationType organizationType;
 
   public AOrganization() {}
 
