c6839ae200a9711ee34c0d1c2ed96fffe1e01ec1
NicoGGG <nicolas.guelfi@achilio.com>
2022-04-06 13:55:01 +0200

    add dataset mock helper

--- src/test/java/com/achilio/mvm/service/MockHelper.java
+++ src/test/java/com/achilio/mvm/service/MockHelper.java
@@ -4,7 +4,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.achilio.mvm.service.entities.AColumn;
+import com.achilio.mvm.service.entities.ADataset;
 import com.achilio.mvm.service.entities.ATable;
+import com.achilio.mvm.service.entities.Project;
 import com.achilio.mvm.service.entities.Query;
 import com.achilio.mvm.service.visitors.ATableId;
 import java.util.Collections;
@@ -13,6 +15,14 @@ import java.util.List;
 public class MockHelper {
 
   private static final String DEFAULT_PROJECT_ID = "myproject";
+
+  public static ADataset datasetMock(Project project, String datasetName) {
+    ADataset mock = mock(ADataset.class);
+    when(mock.getProject()).thenReturn(project);
+    when(mock.getDatasetName()).thenReturn(datasetName).thenReturn(datasetName);
+    when(mock.getDatasetId()).thenReturn(project.getProjectId() + "." + datasetName);
+    return mock;
+  }
 
   public static ATable tableMock(ATableId tableId) {
     return tableMock(tableId, Collections.emptyList());
