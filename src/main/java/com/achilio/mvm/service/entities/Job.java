4f127122ef7af875713e11eedfdf521f195e25b1
NicoGGG <nicolas.guelfi@achilio.com>
2022-04-09 16:55:27 +0200

    rename jobs table

--- src/main/java/com/achilio/mvm/service/entities/Job.java
+++ src/main/java/com/achilio/mvm/service/entities/Job.java
@@ -21,7 +21,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Setter
 @Getter
 @Entity
-@Table(name = "fetcher_jobs")
+@Table(name = "jobs")
 @EnableJpaAuditing
 @EntityListeners(AuditingEntityListener.class)
 public class Job {
