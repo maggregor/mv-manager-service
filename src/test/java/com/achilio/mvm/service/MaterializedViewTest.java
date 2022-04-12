e8818a45b0a67824f7b8095abef60824ea7c8f1c
NicoGGG <nicolas.guelfi@achilio.com>
2022-04-12 12:54:24 +0200

    MV equals and hash tests

--- src/test/java/com/achilio/mvm/service/MaterializedViewTest.java
+++ src/test/java/com/achilio/mvm/service/MaterializedViewTest.java
@@ -2,6 +2,7 @@ package com.achilio.mvm.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.achilio.mvm.service.entities.FindMVJob;
@@ -18,23 +19,27 @@ public class MaterializedViewTest {
 
   private static final String PROJECT_ID = "projectId1";
   private static final String DATASET_NAME = "datasetName";
-  private static final String TABLE_NAME = "tableName";
-  private static final String STATEMENT = "SELECT 1";
-  private static final String HASH_CODE = String.valueOf(Math.abs(STATEMENT.hashCode()));
-  private static final FindMVJob job = new FindMVJob(PROJECT_ID, 7);
-  private static final ATableId tableId = ATableId.of(PROJECT_ID, DATASET_NAME, TABLE_NAME);
+  private static final String TABLE_NAME1 = "tableName1";
+  private static final String TABLE_NAME2 = "tableName2";
+  private static final String STATEMENT1 = "SELECT 1";
+  private static final String STATEMENT2 = "SELECT 2";
+  private static final String HASH_CODE = String.valueOf(Math.abs(STATEMENT1.hashCode()));
+  private static final FindMVJob JOB1 = new FindMVJob(PROJECT_ID, 7);
+  private static final FindMVJob JOB2 = new FindMVJob(PROJECT_ID, 14);
+  private static final ATableId TABLE_ID1 = ATableId.of(PROJECT_ID, DATASET_NAME, TABLE_NAME1);
+  private static final ATableId TABLE_ID2 = ATableId.of(PROJECT_ID, DATASET_NAME, TABLE_NAME2);
 
   @Test
   public void simpleConstructorValidation() {
-    MaterializedView mv = new MaterializedView(job, tableId, STATEMENT);
-    assertEquals(job, mv.getInitialJob());
-    assertEquals(job, mv.getLastJob());
+    MaterializedView mv = new MaterializedView(JOB1, TABLE_ID1, STATEMENT1);
+    assertEquals(JOB1, mv.getInitialJob());
+    assertEquals(JOB1, mv.getLastJob());
     assertEquals(PROJECT_ID, mv.getProjectId());
     assertEquals(DATASET_NAME, mv.getDatasetName());
-    assertEquals(TABLE_NAME, mv.getTableName());
+    assertEquals(TABLE_NAME1, mv.getTableName());
     assertEquals(MVStatus.NOT_APPLIED, mv.getStatus());
     assertEquals(MVStatusReason.WAITING_APPROVAL, mv.getStatusReason());
-    assertEquals(STATEMENT, mv.getStatement());
+    assertEquals(STATEMENT1, mv.getStatement());
     assertEquals("tableName_achilio_mv_" + HASH_CODE, mv.getMvName());
     assertEquals("achilio_mv_" + HASH_CODE, mv.getMvDisplayName());
     assertEquals(Integer.valueOf(0), mv.getHits());
@@ -44,5 +49,27 @@ public class MaterializedViewTest {
     assertTrue(mv.isApplied());
     mv.setStatus(MVStatus.OUTDATED);
     assertTrue(mv.isApplied());
+  }
+
+  @Test
+  public void equalsAndHashCode() {
+    MaterializedView mv1 = new MaterializedView(JOB1, TABLE_ID1, STATEMENT1);
+    MaterializedView mv2 = new MaterializedView(JOB1, TABLE_ID1, STATEMENT1);
+    MaterializedView mv3 = new MaterializedView(JOB1, TABLE_ID1, STATEMENT2);
+    MaterializedView mv4 = new MaterializedView(JOB1, TABLE_ID2, STATEMENT2);
+    MaterializedView mv5 = new MaterializedView(JOB2, TABLE_ID1, STATEMENT1);
+    assertEquals(mv1, mv1);
+    assertEquals(mv1, mv2);
+    assertEquals(mv1, mv5);
+    assertNotEquals("notAView", mv1);
+    assertNotEquals(mv1, mv3);
+    assertNotEquals(mv3, mv4);
+
+    assertEquals(mv1.hashCode(), mv1.hashCode());
+    assertEquals(mv1.hashCode(), mv2.hashCode());
+    assertEquals(mv1.hashCode(), mv5.hashCode());
+    assertNotEquals("notAView", mv1.hashCode());
+    assertNotEquals(mv1.hashCode(), mv3.hashCode());
+    assertNotEquals(mv3.hashCode(), mv4.hashCode());
   }
 }
