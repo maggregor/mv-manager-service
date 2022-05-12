fca1ab16235046b66c5d640e1d1973b4d50ea23a
estebgonza <esteban.gonzalez@achilio.com>
2022-05-12 20:34:23 +0200

    support and add config

--- src/main/java/com/achilio/mvm/service/services/GooglePubSubService.java
+++ src/main/java/com/achilio/mvm/service/services/GooglePubSubService.java
@@ -6,13 +6,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutureCallback;
 import com.google.api.core.ApiFutures;
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.GrpcTransportChannel;
+import com.google.api.gax.rpc.FixedTransportChannelProvider;
+import com.google.api.gax.rpc.TransportChannelProvider;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import org.codehaus.plexus.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,22 +37,26 @@ public class GooglePubSubService implements PublisherService {
   private static final String ATTRIBUTE_EVENT_TYPE = "eventType";
 
   private final ObjectMapper objectMapper = new ObjectMapper();
-  private final boolean PUBLISHER_ENABLED;
-  private final TopicName SSE_TOPIC_ID;
+  private final boolean publisherEnabled;
+  private final TopicName sseTopicId;
+  private final String emulatorHost;
 
   @Autowired
   public GooglePubSubService(
       @Value("${application.google-project-id}") String publisherProjectId,
       @Value("${publisher.enabled}") boolean publisherEnabled,
+      @Value("${publisher.emulator.host}") String emulatorHost,
       @Value("${publisher.sse-topic-id}") String sseTopicId) {
-    this.PUBLISHER_ENABLED = publisherEnabled;
-    this.SSE_TOPIC_ID = TopicName.of(publisherProjectId, sseTopicId);
-    LOGGER.info("Initialized publisher: PROJECT_ID: {}, SSE: {}", publisherProjectId, SSE_TOPIC_ID);
+    this.publisherEnabled = publisherEnabled;
+    this.sseTopicId = TopicName.of(publisherProjectId, sseTopicId);
+    this.emulatorHost = emulatorHost;
+    LOGGER.info("Initialized publisher: PROJECT_ID: {}, SSE: {}", publisherProjectId,
+        this.sseTopicId);
   }
 
   @Override
   public void handleEvent(Event event) {
-    if (!PUBLISHER_ENABLED) {
+    if (!publisherEnabled) {
       LOGGER.info("Event {} not published: publisher is disabled", event.getEventType());
       return;
     }
@@ -66,7 +78,7 @@ public class GooglePubSubService implements PublisherService {
   private void publishMessage(PubsubMessage pubsubMessage) {
     Publisher publisher = null;
     try {
-      publisher = Publisher.newBuilder(SSE_TOPIC_ID).build();
+      publisher = createPublisherInstance();
 
       ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
       ApiFutures.addCallback(messageIdFuture, new ApiFutureCallback<String>() {
@@ -89,6 +101,26 @@ public class GooglePubSubService implements PublisherService {
         }
       }
     }
+  }
+
+  /**
+   * Creates the dedicated publisher instance. Enable emulator if PUBLISHER_EMULATOR_HOST is not
+   * empty.
+   *
+   * @return
+   * @throws IOException
+   */
+  private Publisher createPublisherInstance() throws IOException {
+    Publisher.Builder builder = Publisher.newBuilder(sseTopicId);
+    if (StringUtils.isNotEmpty(emulatorHost)) {
+      ManagedChannel channel = ManagedChannelBuilder.forTarget(emulatorHost).usePlaintext().build();
+      TransportChannelProvider channelProvider =
+          FixedTransportChannelProvider.create(GrpcTransportChannel.create(channel));
+      CredentialsProvider credentialsProvider = NoCredentialsProvider.create();
+      builder.setCredentialsProvider(credentialsProvider);
+      builder.setChannelProvider(channelProvider);
+    }
+    return builder.build();
   }
 
 }
